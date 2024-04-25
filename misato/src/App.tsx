fb1386e55a29ab27b4a29fda7c603aee17602068
GOMI9999 <168045713+GOMI9999@users.noreply.github.com>
2024-04-24 19:42:26 -0600

    first sesh

---
+++ misato/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/images/7551cbad-15e3-47e9-948f-ace82ff224b5.jpg" alt="Description" />
+    </div>
+  );
+}
+
+export default App;
---
+++ misato/src/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+
+
+
+function Navbar() {
+  return (
+    <div className="Nav"><h1>Mommy</h1></div>
+    
+  );
+}
+
+export default Navbar;
